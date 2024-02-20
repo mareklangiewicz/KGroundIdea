60b2049a35d0ebdcc899d0f6568d2b9f3855e93f
GitHub Action <action@github.com>
2024-02-20 17:14:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/mareklangiewicz/kgroundidea/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/mareklangiewicz/kgroundidea/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.mareklangiewicz.kgroundidea.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.mareklangiewicz.kgroundidea.MyBundle
+import com.github.mareklangiewicz.kgroundidea.services.MyProjectService
 import javax.swing.JButton
 
 
